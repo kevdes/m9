1b01d4984e5cf5eb01a2a0a95cef75e00699c5c8
Kevin <kevin.desa@gmail.com>
2025-03-27 18:18:18 +1100

    Update android_build_env.sh

--- .circleci/android_build_env.sh
+++ .circleci/android_build_env.sh
@@ -35,6 +35,7 @@ fi
 DEBIAN_FRONTEND=noninteractive \
     apt install \
     adb autoconf automake axel bc bison build-essential \
+    libelf-dev \
     ccache clang cmake curl expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git git-lfs gnupg gperf \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
